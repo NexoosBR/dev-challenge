096cdec3eb4e661f3390df65834f44eac3be095d
Lukas Pol <lukaspaespol@gmail.com>
2020-10-30 18:44:49 -0300

    check cnpj in company

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,4 +3,11 @@ class Company < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validate :check_cnpj
+
+  private
+    def check_cnpj
+      errors.add(:add, I18n.t(:invalid_cnpj)) unless CNPJ.valid?(self.cnpj)
+    end
 end
