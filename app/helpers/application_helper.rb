590515da9cfd7812c65c366606156de5fc32f62c
Lukas Pol <lukaspaespol@gmail.com>
2020-10-30 19:31:00 -0300

    added new helpers and partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
 module ApplicationHelper
+  def title(title_string)
+    content_for :title do
+      title_string
+    end
+  end
+
+  def link_to_back(path)
+    render 'shared/link_to_back', path: path
+  end
+
+  def link_to_new_resource(path, title = nil)
+    render 'shared/link_to_new_resource', title: title, path: path
+  end
+
+  def link_to_edit_resource(path, title = nil)
+    render 'shared/link_to_edit_resource', title: title, path: path
+  end
+
+  def link_to_detail_resource(path, title = nil)
+    render 'shared/link_to_detail_resource', title: title, path: path
+  end
+
+  def boolean_label(boolean)
+    boolean == true ? t(:yep) : t(:nope)
+  end
 end
