fa3f9eecfe3fec3b68d94f1b65101de77b94d0c9
Lukas Pol <lukaspaespol@gmail.com>
2020-10-30 18:33:06 -0300

    setup spec with factory

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,18 @@ module Myapp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # don't generate RSpec tests for views and helpers
+    config.generators do |g|
+      g.test_framework :rspec
+      g.view_specs false
+      g.helper_specs false
+      g.assets false
+      g.helper false
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
+    end
+
+    config.time_zone = 'Brasilia'
+    config.i18n.default_locale = 'pt-BR'
   end
 end
