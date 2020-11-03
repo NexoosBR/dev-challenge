aa259a901740ef156420e88cb2aa2b2186ab25f7
Lukas Pol <lukaspaespol@gmail.com>
2020-11-03 18:30:09 -0300

    config multiples phone numbers

--- app/controllers/phone_numbers_controller.rb
+++ app/controllers/phone_numbers_controller.rb
@@ -2,19 +2,17 @@ class PhoneNumbersController < ApplicationController
   before_action :set_phone_number, only: [:show, :edit, :update, :destroy]
 
   # GET /phone_numbers
-  # GET /phone_numbers.json
   def index
-    @phone_numbers = PhoneNumber.all
+    @phone_numbers = current_company.phone_numbers.all
   end
 
   # GET /phone_numbers/1
-  # GET /phone_numbers/1.json
   def show
   end
 
   # GET /phone_numbers/new
   def new
-    @phone_number = PhoneNumber.new
+    @phone_number = current_company.phone_numbers.new
   end
 
   # GET /phone_numbers/1/edit
@@ -22,43 +20,29 @@ class PhoneNumbersController < ApplicationController
   end
 
   # POST /phone_numbers
-  # POST /phone_numbers.json
   def create
-    @phone_number = PhoneNumber.new(phone_number_params)
+    @phone_number = current_company.phone_numbers.new(phone_number_params)
 
-    respond_to do |format|
-      if @phone_number.save
-        format.html { redirect_to @phone_number, notice: 'Phone number was successfully created.' }
-        format.json { render :show, status: :created, location: @phone_number }
-      else
-        format.html { render :new }
-        format.json { render json: @phone_number.errors, status: :unprocessable_entity }
-      end
+    if @phone_number.save
+      redirect_to @phone_number, notice: t(:created, model: t(:phone_number, scope: 'activerecord.models'))
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /phone_numbers/1
-  # PATCH/PUT /phone_numbers/1.json
   def update
-    respond_to do |format|
-      if @phone_number.update(phone_number_params)
-        format.html { redirect_to @phone_number, notice: 'Phone number was successfully updated.' }
-        format.json { render :show, status: :ok, location: @phone_number }
-      else
-        format.html { render :edit }
-        format.json { render json: @phone_number.errors, status: :unprocessable_entity }
-      end
+    if @phone_number.update(phone_number_params)
+      redirect_to @phone_number, notice: t(:updated, model: t(:phone_number, scope: 'activerecord.models'))
+    else
+      render :edit
     end
   end
 
   # DELETE /phone_numbers/1
-  # DELETE /phone_numbers/1.json
   def destroy
     @phone_number.destroy
-    respond_to do |format|
-      format.html { redirect_to phone_numbers_url, notice: 'Phone number was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to phone_numbers_url, notice: t(:deleted, model: t(:phone_number, scope: 'activerecord.models'))
   end
 
   private
