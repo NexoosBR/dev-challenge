c9d234a895721258ef203ff532ce5e4d858eeb31
Lukas Pol <lukaspaespol@gmail.com>
2020-11-03 20:17:05 -0300

    fixing tests

--- spec/models/installment_spec.rb
+++ spec/models/installment_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Installment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:company) { create(:company) }
+  let(:loan) { create(:loan, company: company) }
+  let(:installment) { Installment.first }
+
+  context 'validates loan' do
+    it 'should loan.pmt is equal installment.value is valid' do
+      expect(loan.pmt).to eq(installment.value)
+    end
+
+    it 'should loan.number_installments equal to installment quantity' do
+      expect(loan.number_installments).to eq(Installment.count)
+    end
+  end
 end
