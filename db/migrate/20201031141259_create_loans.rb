8512dea97bc912a545dba8dffd4d533ba545f68d
Lukas Pol <lukaspaespol@gmail.com>
2020-11-03 17:46:40 -0300

    change value_installments to pmt

--- db/migrate/20201031141259_create_loans.rb
+++ db/migrate/20201031141259_create_loans.rb
@@ -4,7 +4,7 @@ class CreateLoans < ActiveRecord::Migration[6.0]
       t.decimal :value
       t.decimal :rate
       t.integer :number_installments
-      t.decimal :value_installments
+      t.decimal :pmt
       t.references :company, null: false, foreign_key: true
 
       t.timestamps
