2db9d6966553741354beb0e48f87eadf6c838a8f
Gerson Nascimento <gerson.nascimento@locaweb.com.br>
2020-11-21 12:34:54 -0300

    Add approve and deny method into credit_request model

--- app/models/credit_request.rb
+++ app/models/credit_request.rb
@@ -2,8 +2,17 @@ class CreditRequest < ApplicationRecord
   has_many :payments
   belongs_to :company
 
+  enum status: [ :awaiting_approval, :approved, :denied ]
+
   before_create do
     self.status = 0
   end
 
+  def approve
+    approved!
+  end
+
+  def deny
+    denied!
+  end
 end
