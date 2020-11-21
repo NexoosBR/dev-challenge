21372d44948e449d367c71cf1ee2a5dddc1f91f6
Gerson Nascimento <gerson.nascimento@locaweb.com.br>
2020-11-21 15:17:26 -0300

    Create approve credit request feature

--- app/controllers/credit_requests_controller.rb
+++ app/controllers/credit_requests_controller.rb
@@ -1,4 +1,6 @@
 class CreditRequestsController < ApplicationController
+  after_action :create_payments, only: [:approve]
+
   def show
     @credit_request = CreditRequest.find(params[:id])
   end
@@ -9,7 +11,19 @@ class CreditRequestsController < ApplicationController
     redirect_to @request
   end
 
+  def approve
+    @request = CreditRequest.find(params[:id])
+    @request.approved!
+    flash[:success] = 'Solicitação aprovada'
+
+    redirect_to @request
+  end
+
   private
+
+  def create_payments
+    PaymentsGenerator.new.generate_for_credit_request(@request)
+  end
 
   def credit_request_params
     params.require(:credit_request).permit(:amount, :periods, :monthly_fee, :company_id, :monthly_value)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root to: "home#index"
 
-  resources :credit_requests, only: [:show, :create]
+  resources :credit_requests, only: [:show, :create] do
+    member do
+      put 'approve', to: 'credit_requests#approve'
+    end
+  end
 
   namespace :api do
     get 'calculate_pmt', to: 'credit_requests#calculate_pmt'
