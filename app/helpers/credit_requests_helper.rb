18e8370d80d073b2c87ca0e6c6032bed6fb08d1b
Gerson Nascimento <gerson.nascimento@locaweb.com.br>
2020-11-23 23:04:36 -0300

    Improve views

--- app/helpers/credit_requests_helper.rb
+++ app/helpers/credit_requests_helper.rb
@@ -2,4 +2,15 @@ module CreditRequestsHelper
   def available_periods
     MonthlyFeeVerifier.new.available_periods
   end
+
+  def list_payments(credit_request)
+    return render('no_payment_found') if credit_request.payments.empty?
+    render('payment_list')
+  end
+
+  def show_approve_button_for(credit_request)
+    return if credit_request.approved?
+
+    link_to('Aprovar', approve_credit_request_path(credit_request), method: :put)
+  end
 end
--- spec/helpers/credit_requests_helper_spec.rb
+++ spec/helpers/credit_requests_helper_spec.rb
@@ -13,4 +13,25 @@ describe CreditRequestsHelper do
       expect(subject).to eq(expected_response)
     end
   end
+
+  context 'When showing approve link' do
+    it 'does return formed link' do
+      credit_request = create(:credit_request)
+
+      subject = helper.show_approve_button_for(credit_request)
+
+      expect(subject).to include('Aprovar')
+    end
+  end
+
+  context 'When trying to show approve link for an approved request' do
+    it 'does return nil' do
+      credit_request = create(:credit_request)
+
+      credit_request.approved!
+      subject = helper.show_approve_button_for(credit_request)
+
+      expect(subject).to be_nil
+    end
+  end
 end
