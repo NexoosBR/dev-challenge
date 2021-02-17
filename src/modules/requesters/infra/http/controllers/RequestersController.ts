a4962720e20825f88b089f4605ac98fce85510aa
Marcelo <marcelo.brasil.amorim@gmail.com>
2021-02-16 23:17:18 -0300

    feat: changes fake Requester repository to typeorm implementation

--- src/modules/requesters/infra/http/controllers/RequestersController.ts
+++ src/modules/requesters/infra/http/controllers/RequestersController.ts
@@ -1,16 +1,15 @@
-import FakeRequestersRepository from '@modules/requesters/repositories/fakes/FakeRequestersRepository';
-import CreateRequesterService from '@modules/requesters/services/CreateRequesterService';
 import { Request, Response } from 'express';
+
+import CreateRequesterService from '@modules/requesters/services/CreateRequesterService';
+import RequestersRepository from '../../typeorm/repositories/RequesterRepository';
 
 export default class RequestersController {
   public async create(request: Request, response: Response): Promise<Response> {
     const { companyName, cnpj, address, phone } = request.body;
 
-    const fakeRequestersRepository = new FakeRequestersRepository();
+    const requestersRepository = new RequestersRepository();
 
-    const createRequester = new CreateRequesterService(
-      fakeRequestersRepository,
-    );
+    const createRequester = new CreateRequesterService(requestersRepository);
 
     const requester = await createRequester.execute({
       companyName,
