0a8df4dd98fb4549a695e177e613556378939bb9
Marcelo <marcelo.brasil.amorim@gmail.com>
2021-02-16 23:19:16 -0300

    chore: adds dist directory to gitignore

--- dist/src/routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var routes = express_1.Router();
-routes.get('/', function (request, response) {
-    return response.json({ message: ' Hello world.' });
-});
-exports.default = routes;
