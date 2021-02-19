7ce47c7f9042f8a00496edc1d9dad9769f2fa1e8
Samuel <lima.samuel81@gmail.com>
2021-02-18 23:34:27 -0300

    chore: add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    jest: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'standard'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    'no-useless-constructor': 'off'
+  }
+}
