d36a9b0874eaaa4f1385dcf6fea68fbcb632f71f
Andre Cesar Gomes <andre.cesar@rebase.com.br>
2021-03-02 11:01:22 -0300

    React + Webpacker initial setup

---
+++ app/javascript/src/app.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+
+const Hello = (props) => <div>Hello {props.name}!</div>;
+
+Hello.defaultProps = {
+  name: "David",
+};
+
+Hello.propTypes = {
+  name: PropTypes.string,
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(
+    <Hello name="React" />,
+    document.getElementById("react-mount-point")
+  );
+});
