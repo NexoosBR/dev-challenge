d2d1b86d7d61157444be30b76370e0ff60a93498
Gabriel Barboza <gabrielmbarboza@gmail.com>
2021-03-09 17:09:04 -0300

    Remove Phone

--- spec/factories/phones.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :phone do
-    area_code { Faker::Number.number(digits: 2) }
-    phone_type { 0 }
-    phone_number { Faker::Number.number(digits: 8) }
-    borrower
-  end
-end
